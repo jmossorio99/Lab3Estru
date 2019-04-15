4dfe336d07f830273a86a78a430942d31864f50f
Jose <joseossorio99@hotmail.com>
2019-04-15 14:13:57 -0500

    finished preliminar gui

--- src/controller/StartingWindowController.java
+++ src/controller/StartingWindowController.java
@@ -1,5 +1,69 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
+
 public class StartingWindowController {
+
+	@FXML
+	private TextField initialDateTextField;
+	@FXML
+	private TextField finalDateTextField;
+	@FXML
+	private HBox togglesHBox;
+	@FXML
+	private ToggleButton US30Toggle;
+	@FXML
+	private ToggleButton USSPX500Toggle;
+	@FXML
+	private ToggleButton WTIToggle;
+	@FXML
+	private ToggleButton BTCUSDToggle;
+	@FXML
+	private ToggleButton EURUSDToggle;
+	@FXML
+	private ToggleButton GBPCADToggle;
+	@FXML
+	private ToggleButton USDJPYToggle;
+	@FXML
+	private ToggleButton XAUUSDToggle;
+	@FXML
+	private Label textResultLbl;
+	@FXML
+	private Label resultLbl;
+
+	@FXML
+	void highestPriceBtnPressed(ActionEvent event) {
+
+	}
+
+	@FXML
+	void lowestPriceBtnPressed(ActionEvent event) {
+
+	}
+
+	@FXML
+	void showGraphBtnPressed(ActionEvent event) {
+
+	}
+
+	@FXML
+	void singleHighestGrowthBtnPressed(ActionEvent event) {
+
+	}
+
+	@FXML
+	void stockAboveValueBtnPressed(ActionEvent event) {
+
+	}
+
+	@FXML
+	void threeGreatestGrowthBtnPressed(ActionEvent event) {
+
+	}
 
 }
