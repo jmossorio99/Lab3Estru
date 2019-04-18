f24c68cdfc07fcfba465c33d32f73b63d5614f11
Jose <joseossorio99@hotmail.com>
2019-04-18 18:52:00 -0500

    finished methods for highest growth day, year and month ^^

--- src/model/HighestGrowthFinder.java
+++ src/model/HighestGrowthFinder.java
@@ -28,13 +28,12 @@ public class HighestGrowthFinder {
 		double currentPrice = 0;
 		double highest = Double.MIN_VALUE;
 		String highestMonth = "";
-
 		String[] parts = in.readLine().split(",");
-		String[] dateNTime = parts[1].substring(1).split(" ");
+		String[] dateNTime = (parts[1].substring(1)).split(" ");
 		String[] date = dateNTime[0].split("/");
 		String month = date[1];
 		currentMonth = month;
-		double startPrice = Double.parseDouble(parts[2]);
+		double startPrice = Double.parseDouble(parts[2].substring(1));
 		double finalPrice = 0;
 		currentPrice = startPrice;
 
@@ -43,27 +42,126 @@ public class HighestGrowthFinder {
 			String line = in.readLine();
 			if (line != null) {
 				parts = line.split(",");
-				dateNTime = parts[1].substring(1).split(" ");
+				dateNTime = (parts[1].substring(1)).split(" ");
 				date = dateNTime[0].split("/");
 				month = date[1];
 				if (month != currentMonth) {
-					finalPrice = currentPrice;
-					if (finalPrice - startPrice > highest) {
+					if ((finalPrice - startPrice) > highest) {
 						highest = finalPrice - startPrice;
 						highestMonth = currentMonth;
 						currentMonth = month;
 					}
-					currentPrice = Double.parseDouble(parts[2]);
+					finalPrice = currentPrice;
+					currentPrice = Double.parseDouble(parts[2].substring(1));
 					startPrice = currentPrice;
 				} else {
-					currentPrice = Double.parseDouble(parts[2]);
+					currentPrice = Double.parseDouble(parts[2].substring(1));
 				}
 			} else {
 				finished = true;
 			}
 
 		}
+		in.close();
 		return highestMonth;
+	}
+
+	public String findHighestGrowthDay() throws IOException {
+
+		boolean finished = false;
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+		String currentDay = "";
+		double currentPrice = 0;
+		double highest = Double.MIN_VALUE;
+		String highestDay = "";
+		String[] parts = in.readLine().split(",");
+		String[] dateNTime = (parts[1].substring(1)).split(" ");
+		String[] date = dateNTime[0].split("/");
+		String day = date[0];
+		currentDay = day;
+		double startPrice = Double.parseDouble(parts[2].substring(1));
+		double finalPrice = 0;
+		currentPrice = startPrice;
+
+		while (!finished) {
+
+			String line = in.readLine();
+			if (line != null) {
+				parts = line.split(",");
+				dateNTime = (parts[1].substring(1)).split(" ");
+				date = dateNTime[0].split("/");
+				day = date[0];
+				if (day != currentDay) {
+					if ((finalPrice - startPrice) > highest) {
+						highest = finalPrice - startPrice;
+						highestDay = currentDay;
+						currentDay = day;
+					}
+					finalPrice = currentPrice;
+					currentPrice = Double.parseDouble(parts[2].substring(1));
+					startPrice = currentPrice;
+				} else {
+					currentPrice = Double.parseDouble(parts[2].substring(1));
+				}
+			} else {
+				finished = true;
+			}
+
+		}
+		in.close();
+		return highestDay;
+
+	}
+
+	public String findHighestGrowthYear() throws IOException {
+
+		boolean finished = false;
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+		String currentYear = "";
+		double currentPrice = 0;
+		double highest = Double.MIN_VALUE;
+		String highestYear = "";
+		String[] parts = in.readLine().split(",");
+		String[] dateNTime = (parts[1].substring(1)).split(" ");
+		String[] date = dateNTime[0].split("/");
+		String year = date[2];
+		currentYear = year;
+		double startPrice = Double.parseDouble(parts[2].substring(1));
+		double finalPrice = 0;
+		currentPrice = startPrice;
+
+		while (!finished) {
+
+			String line = in.readLine();
+			if (line != null) {
+				parts = line.split(",");
+				dateNTime = (parts[1].substring(1)).split(" ");
+				date = dateNTime[0].split("/");
+				year = date[2];
+				if (year != currentYear) {
+					if ((finalPrice - startPrice) > highest) {
+						highest = finalPrice - startPrice;
+						highestYear = currentYear;
+						currentYear = year;
+					}
+					finalPrice = currentPrice;
+					currentPrice = Double.parseDouble(parts[2].substring(1));
+					startPrice = currentPrice;
+				} else {
+					currentPrice = Double.parseDouble(parts[2].substring(1));
+				}
+			} else {
+				finished = true;
+			}
+
+		}
+		in.close();
+		return highestYear;
+
 	}
 
 }
