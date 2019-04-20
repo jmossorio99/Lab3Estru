b5df2f2259d6aac67ee0324d6dcb9af62e3db3d1
Marco Perez <marcopza99@hotmail.com>
2019-04-20 14:23:59 -0500

    Tests for creation, insertion and fixups in an avl tree

--- src/tests/AVLTreeTest.java
+++ src/tests/AVLTreeTest.java
@@ -20,12 +20,31 @@ public class AVLTreeTest {
 	}
 	
 	@Test
-	private void testCreation() {
+	void testCreation() {
 		setUpScenario1();
-		assertNotNull(avl);
+		assertNotNull(avl.getRoot());
 	}
 	
-	private void testInsertion() {
-		
+	@Test
+	void testInsertion() {
+		setUpScenario2();
+		assertTrue(new AVLNode<Integer>(6, null, null).compareTo(avl.getRoot()) == 0);
+		assertTrue(new AVLNode<Integer>(5, null, null).compareTo(avl.getRoot().getLeft()) == 0);
+		assertTrue(new AVLNode<Integer>(8, null, null).compareTo(avl.getRoot().getRight()) == 0);
+	}
+	
+	@Test
+	void testFixUps() {
+		setUpScenario2();
+		avl.insert(10, null, null);
+		avl.insert(4, null, null);
+		avl.insert(3, null, null);
+		assertTrue(new AVLNode<Integer>(6, null, null).compareTo(avl.getRoot()) == 0);
+		assertTrue(new AVLNode<Integer>(4, null, null).compareTo(avl.getRoot().getLeft()) == 0);
+		assertTrue(new AVLNode<Integer>(8, null, null).compareTo(avl.getRoot().getRight()) == 0);
+		assertTrue(new AVLNode<Integer>(10, null, null).compareTo(avl.getRoot().getRight().getRight()) == 0);
+		assertNull(avl.getRoot().getRight().getLeft());
+		assertTrue(new AVLNode<Integer>(3, null, null).compareTo(avl.getRoot().getLeft().getLeft()) == 0);
+		assertTrue(new AVLNode<Integer>(5, null, null).compareTo(avl.getRoot().getLeft().getRight()) == 0);
 	}
 }
