02e446bc96e613e4f8f2dc26a682cf71327872a2
Marco Perez <marcopza99@hotmail.com>
2019-04-20 13:58:43 -0500

    Scenarios of the AVLTree Tests

--- src/tests/AVLTreeTest.java
+++ src/tests/AVLTreeTest.java
@@ -1,5 +1,31 @@
 package tests;
 
+import model.AVLNode;
+import model.AVLTree;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class AVLTreeTest {
 
+	private AVLTree<Integer> avl;
+	
+	private void setUpScenario1() {
+		avl = new AVLTree<Integer>(new AVLNode<Integer>(new Integer(5), null, null));
+	}
+	
+	private void setUpScenario2() {
+		setUpScenario1();
+		avl.insert(new Integer(8), null, null);
+		avl.insert(new Integer(6), null, null);
+	}
+	
+	@Test
+	private void testCreation() {
+		setUpScenario1();
+		assertNotNull(avl);
+	}
+	
+	private void testInsertion() {
+		
+	}
 }
