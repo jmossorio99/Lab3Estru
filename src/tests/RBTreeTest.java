5bfe10787900cc506f4a6baeca1fc76ee762f17a
Marco Perez <marcopza99@hotmail.com>
2019-04-20 19:47:56 -0500

    Tested Coloring, insertion and creation in RBTrees

--- src/tests/RBTreeTest.java
+++ src/tests/RBTreeTest.java
@@ -16,12 +16,43 @@ public class RBTreeTest {
 	}
 	
 	private void setUpScenario2() {
-		
+		setUpScenario1();
+		rb.insert(new RBNode<Integer>(14, null, null));
+		rb.insert(new RBNode<Integer>(2, null, null));
+	}
+	
+	private void setUpScenario3() {
+		setUpScenario2();
+		rb.insert(new RBNode<Integer>(15, null, null));
+		rb.insert(new RBNode<Integer>(1, null, null));
+		rb.insert(new RBNode<Integer>(7, null, null));
+		rb.insert(new RBNode<Integer>(5, null, null));
+		rb.insert(new RBNode<Integer>(8, null, null));
 	}
 	
 	@Test
 	void testCreation() {
 		setUpScenario1();
 		assertNotNull(rb.getRoot());
+		assertEquals(RBTree.BLACK, rb.getRoot().getColor());
+	}
+	
+	@Test
+	void testInsertion() {
+		setUpScenario2();
+		assertTrue(rb.getRoot().getRight().compareTo(new RBNode<Integer>(14, null, null)) == 0);
+		assertTrue(rb.getRoot().getLeft().compareTo(new RBNode<Integer>(2, null, null)) == 0);
+	}
+	
+	@Test
+	void testColoring() {
+		setUpScenario3();
+		assertEquals(RBTree.BLACK, rb.getRoot().getColor());
+		assertEquals(RBTree.BLACK, rb.getRoot().getRight().getColor());
+		assertEquals(RBTree.BLACK, rb.getRoot().getLeft().getLeft().getColor());
+		assertEquals(RBTree.BLACK, rb.getRoot().getLeft().getRight().getColor());
+		assertEquals(RBTree.RED, rb.getRoot().getLeft().getColor());
+		assertEquals(RBTree.RED, rb.getRoot().getLeft().getRight().getLeft().getColor());
+		assertEquals(RBTree.RED, rb.getRoot().getLeft().getRight().getLeft().getColor());
 	}
 }
